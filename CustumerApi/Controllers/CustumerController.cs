7a8318e6fbbb4912afd52f5825e5b2cbc47d87c6
Hans Albeiro Torres Rojas <hantorr@finandina.corp>
2019-09-02 11:37:43 -0500

    Apis

---
+++ CustumerApi/Controllers/CustumerController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CustumerApi.Controllers
+{
+    [Route("Esb/TaxId")]
+    [ApiController]
+    public class CustumerController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet, Route("")]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "Hans", "Torres", "Vive" };
+        }
+
+    }
+}
